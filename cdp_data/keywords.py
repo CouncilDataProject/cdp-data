8b59baf6b272f7ca52790d693d4a344f945bb995
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2022-04-04 17:49:59 -0700

    More work on relevancy over time - need usage over time

--- cdp_data/keywords.py
+++ cdp_data/keywords.py
@@ -34,14 +34,68 @@ def _stem_n_gram(n_gram: str) -> str:
     # Split and stem each
     return " ".join([stemmer.stem(span) for span in n_gram.split()])
 
+# TODO:
+# Write function to compute full ngram history not "relevancy over time"
+#
+# From Google Ngram Viewer:
+# https://books.google.com/ngrams/info
+# This shows trends in three ngrams from 1960 to 2015: "nursery school"
+# (a 2-gram or bigram), "kindergarten" (a 1-gram or unigram), and
+# "child care" (another bigram). What the y-axis shows is this: of all the bigrams
+# contained in our sample of books written in English and published in the
+# United States, what percentage of them are "nursery school" or "child care"?
+# Of all the unigrams, what percentage of them are "kindergarten"? Here, you can
+# see that use of the phrase "child care" started to rise in the late 1960s,
+# overtaking "nursery school" around 1970 and then "kindergarten" around 1973.
+# It peaked shortly after 1990 and has been falling steadily since.
+#
+# TF-IDF shows the deviation from normal whereas percent of total shows
+# trends in total discussion of Ngram
+ 
 
-def get_ngram_usage(
-    infrastructure_slug: str,
+def get_ngram_relevancy_history(
     ngram: str,
+    infrastructure_slug: str,
     strict: bool = False,
     start_datetime: Optional[Union[str, datetime]] = None,
     end_datetime: Optional[Union[str, datetime]] = None,
 ) -> pd.DataFrame:
+    """
+    Pull an n-gram's relevancy history from a CDP database.
+
+    Parameters
+    ----------
+    ngram: str
+        The unigram, bigram, or trigram to retrieve history for.
+    infrastructure_slug: str
+        The CDP infrastructure slug to connect to.
+    strict: bool
+        Should the provided ngram be used for a strict "unstemmed_gram" query or not.
+        Default: False (stem and clean the ngram before querying)
+    start_datetime: Optional[Union[str, datetime]]
+        The earliest possible datetime for ngram history to be retrieved for.
+        If provided as a string, the datetime should be in ISO format.
+    end_datetime: Optional[Union[str, datetime]]
+        The latest possible datetime for ngram history to be retrieved for.
+        If provided as a string, the datetime should be in ISO format.
+
+    Returns
+    -------
+    ngram_history: pd.DataFrame
+        A pandas DataFrame of all IndexedEventGrams that match the provided ngram query
+        (stemmed or unstemmed).
+
+    Notes
+    -----
+    This function pulls the TF-IDF (or other future indexed values) score for the
+    provided ngram over time. This is a measure of relevancy to a document and not
+    the same as Google's NGram Viewer which shows what percentage of literature
+    used the term.
+
+    See Also
+    --------
+    cdp_data.keywords.compute_ngram_usage_history
+    """
     # Connect to infra
     fireo.connection(
         client=Client(project=infrastructure_slug, credentials=AnonymousCredentials())
@@ -79,10 +133,11 @@ def get_ngram_usage(
     # Because we pulled from the index, there should never be duplicate
     # event calls so we are safe to just apply the whole df
     try:
+        # Register `pandas.progress_apply` and `pandas.Series.map_apply`
         from tqdm import tqdm
 
-        # Register `pandas.progress_apply` and `pandas.Series.map_apply`
-        tqdm.pandas(desc="Event data attached to ngram usage")
+        tqdm.pandas(desc="Event attachment")
+
         ngram_history["event"] = ngram_history.progress_apply(
             lambda row: row.event_ref.get(),
             axis=1,
@@ -103,3 +158,16 @@ def get_ngram_usage(
     ngram_history = ngram_history.drop(["event_ref"], axis=1)
 
     return ngram_history
+
+def compute_ngram_usage_history(
+    ngram: str,
+    infrastructure_slug: str,
+    strict: bool = False,
+    start_datetime: Optional[Union[str, datetime]] = None,
+    end_datetime: Optional[Union[str, datetime]] = None,
+) -> pd.DataFrame:
+    """
+    Pull transcript and event data for the provided infrastructure
+    and compute ngram usage over time.
+    """
+    pass
