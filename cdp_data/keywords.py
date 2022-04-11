eba51b6fbe9724431795f6793a27d0e8b2a8e5dd
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2022-04-11 00:23:45 -0700

    Working processing and prepartion functions for usage history

--- cdp_data/keywords.py
+++ cdp_data/keywords.py
@@ -170,130 +170,12 @@ def get_ngram_relevancy_history(
     return ngram_history
 
 
-def _count_transcript_grams(
-    processing_params: NgramUsageProcessingParameters,
-    strict: bool,
-) -> pd.DataFrame:
-    # Load transcript
-    with open(processing_params.transcript_path, "r") as open_f:
-        transcript = Transcript.from_json(open_f.read())
-
-    # Start a counter
-    counter: Counter = Counter()
-
-    # For each sentence in transcript,
-    # clean or not (based off strict),
-    # count each cleaned or not gram
-    for sentence in transcript.sentences:
-        # Stopwords removed
-        words = [
-            clean_text(
-                word.text,
-                clean_stop_words=True,
-                clean_emojis=True,
-            )
-            for word in sentence.words
-        ]
-        words = [word for word in words if len(word) > 0]
-
-        # Create ngrams
-        for i in range(1, 4):
-            for gram in ngrams(words, i):
-                if strict:
-                    counter.update([_stem_n_gram(" ".join(gram))])
-                else:
-                    counter.update([" ".join(gram)])
-
-    # Convert to dataframe
-    counts = pd.DataFrame.from_dict(counter, orient="index").reset_index()
-    counts = counts.rename(columns={"index": "ngram", 0: "count"})
-
-    # Add columns
-    counts["session_id"] = processing_params.session_id
-    counts["session_datetime"] = processing_params.session_datetime
-
-    return counts
-
-
-def compute_ngram_usage_history(
+def _prepare_ngram_history_plotting_data(
     data: pd.DataFrame,
-    strict: bool = False,
+    ngram_col: str,
+    value_col: str,
+    dt_col: str,
 ) -> pd.DataFrame:
-    """
-    From a session dataset with transcripts available, compute an ngram's usage history.
-    """
-    # Ensure stopwords are downloaded
-    # Do this once to ensure that we don't enter a race condition
-    # with multiple workers trying to download / read overtop one another
-    # later on.
-    try:
-        from nltk.corpus import stopwords
-
-        stopwords.words("english")
-    except LookupError:
-        import nltk
-
-        nltk.download("stopwords")
-        log.info("Downloaded nltk stopwords")
-        from nltk.corpus import stopwords
-
-        stopwords.words("english")
-
-    # Construct partial for threaded counter func
-    counter_func = partial(_count_transcript_grams, strict=strict)
-
-    # Count all uni, bi, and trigrams in transcripts
-    counts = pd.concat(
-        process_map(
-            counter_func,
-            [
-                NgramUsageProcessingParameters(
-                    session_id=row.id,
-                    session_datetime=row.session_datetime,
-                    transcript_path=row.transcript_path,
-                )
-                for _, row in data.iterrows()
-            ],
-        )
-    )
-    return counts
-
-
-def prepare_ngram_history_plotting_data(
-    data: pd.DataFrame,
-    ngram_col: str = "query_gram",
-    value_col: str = "value",
-    dt_col: str = "event_datetime",
-) -> pd.DataFrame:
-    """
-    Prepare an ngram history DataFrame specifically for plotting.
-    This function will subset the DataFrame to just the provided columns,
-    will only store a single value for each day if there are multiple
-    (keeping the max value), and finally filling all missing days with zero values.
-
-    Parameters
-    ----------
-    data: pd.DataFrame
-        The data to prepare for plotting.
-    ngram_col: str
-        The column name for which the "ngram" is stored.
-        Default: "query_gram"
-    value_col: str
-        The column name for which the value of each ngram is stored.
-        Default: "value"
-    dt_col: str
-        The column name for which the datetime is stored.
-        Default: "event_datetime"
-
-    Returns
-    -------
-    prepared: pd.DataFrame
-        The subset and missing dates filled DataFrame.
-
-    See Also
-    --------
-    get_ngram_relevancy_history
-    """
     # Select down to just the columns we want
     # Reset index
     # Sort values by datetime
@@ -335,4 +217,193 @@ def prepare_ngram_history_plotting_data(
         .apply(fill_dates)
         .drop(ngram_col, axis=1)
         .reset_index()
+    )
+
+
+def prepare_ngram_relevancy_history_plotting_data(
+    data: pd.DataFrame,
+    ngram_col: str = "query_gram",
+    value_col: str = "value",
+    dt_col: str = "event_datetime",
+) -> pd.DataFrame:
+    """
+    Prepare an ngram history DataFrame specifically for plotting.
+    This function will subset the DataFrame to just the provided columns,
+    will only store a single value for each day if there are multiple
+    (keeping the max value), and finally filling all missing days with zero values.
+
+    Parameters
+    ----------
+    data: pd.DataFrame
+        The data to prepare for plotting.
+    ngram_col: str
+        The column name for which the "ngram" is stored.
+        Default: "query_gram"
+    value_col: str
+        The column name for which the value of each ngram is stored.
+        Default: "value"
+    dt_col: str
+        The column name for which the datetime is stored.
+        Default: "event_datetime"
+
+    Returns
+    -------
+    prepared: pd.DataFrame
+        The subset and missing dates filled DataFrame.
+
+    See Also
+    --------
+    get_ngram_relevancy_history
+    """
+    return _prepare_ngram_history_plotting_data(
+        data=data,
+        ngram_col=ngram_col,
+        value_col=value_col,
+        dt_col=dt_col,
+    )
+
+
+def _count_transcript_grams(
+    processing_params: NgramUsageProcessingParameters,
+    ngram_size: int,
+    strict: bool,
+) -> pd.DataFrame:
+    # Load transcript
+    with open(processing_params.transcript_path, "r") as open_f:
+        transcript = Transcript.from_json(open_f.read())
+
+    # Start a counter
+    counter: Counter = Counter()
+
+    # For each sentence in transcript,
+    # clean or not (based off strict),
+    # count each cleaned or not gram
+    for sentence in transcript.sentences:
+        # Stopwords removed
+        words = [
+            clean_text(
+                word.text,
+                clean_stop_words=True,
+                clean_emojis=True,
+            )
+            for word in sentence.words
+        ]
+        words = [word for word in words if len(word) > 0]
+
+        # Create ngrams
+        for gram in ngrams(words, ngram_size):
+            if not strict:
+                counter.update([_stem_n_gram(" ".join(gram))])
+            else:
+                counter.update([" ".join(gram)])
+
+    # Convert to dataframe
+    counts = pd.DataFrame.from_dict(counter, orient="index").reset_index()
+    counts = counts.rename(columns={"index": "ngram", 0: "count"})
+
+    # Add columns
+    counts["session_id"] = processing_params.session_id
+    counts["session_datetime"] = processing_params.session_datetime
+
+    return counts
+
+
+def compute_ngram_usage_history(
+    data: pd.DataFrame,
+    ngram_size: int = 1,
+    strict: bool = False,
+) -> pd.DataFrame:
+    """
+    From a session dataset with transcripts available, compute an ngram's usage history.
+    """
+    # Ensure stopwords are downloaded
+    # Do this once to ensure that we don't enter a race condition
+    # with multiple workers trying to download / read overtop one another
+    # later on.
+    try:
+        from nltk.corpus import stopwords
+
+        stopwords.words("english")
+    except LookupError:
+        import nltk
+
+        nltk.download("stopwords")
+        log.info("Downloaded nltk stopwords")
+        from nltk.corpus import stopwords
+
+        stopwords.words("english")
+
+    # Construct partial for threaded counter func
+    counter_func = partial(
+        _count_transcript_grams,
+        ngram_size=ngram_size,
+        strict=strict,
+    )
+
+    # TODO:
+    # maybe daskify?
+
+    # Count all uni, bi, and trigrams in transcripts
+    counts = pd.concat(
+        process_map(
+            counter_func,
+            [
+                NgramUsageProcessingParameters(
+                    session_id=row.id,
+                    session_datetime=row.session_datetime,
+                    transcript_path=row.transcript_path,
+                )
+                for _, row in data.iterrows()
+            ],
+        )
+    )
+
+    # Make a column for just date
+    counts["session_date"] = pd.to_datetime(counts["session_datetime"]).dt.date
+
+    # Group by gram and compute combined usage for day
+    counts["day_ngram_count_sum"] = counts.groupby(["ngram", "session_date"])[
+        "count"
+    ].transform("sum")
+
+    # Group by date and compute total words for day
+    counts["day_words_count_sum"] = counts.groupby(["session_date"])["count"].transform(
+        "sum"
+    )
+
+    # Percent of word usage per day
+    counts["day_ngram_percent_usage"] = (
+        counts["day_ngram_count_sum"] / counts["day_words_count_sum"]
+    )
+
+    return counts
+
+
+def prepare_ngram_usage_history_plotting_data(
+    ngram: str,
+    data: pd.DataFrame,
+    strict: bool = False,
+    ngram_col: str = "ngram",
+    percent_col: str = "day_ngram_percent_usage",
+    dt_col: str = "session_date",
+) -> pd.DataFrame:
+    # Prepare ngram for user
+    if not strict:
+        ngram = _stem_n_gram(ngram)
+
+    # Select down to just the ngram we want
+    subset = data.loc[data[ngram_col] == ngram]
+
+    # Check subset length for better error
+    if len(subset) == 0:
+        raise ValueError(
+            f"Provided (or stemmed) ngram ('{ngram}') resulted in "
+            f"zero matching rows for plotting."
+        )
+
+    return _prepare_ngram_history_plotting_data(
+        data=subset,
+        ngram_col=ngram_col,
+        value_col=percent_col,
+        dt_col=dt_col,
     )
