70b3d326ff21936b81f125684d1946b9aad2cfa8
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2022-04-11 17:28:27 -0700

    Lint and format

--- cdp_data/constants.py
+++ cdp_data/constants.py
@@ -3,7 +3,6 @@
 
 from pathlib import Path
 
-
 ###############################################################################
 
 DEFAULT_DATASET_STORAGE_DIR = Path.cwd().resolve() / "cdp-datasets"
