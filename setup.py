2df5ece62fc6e65536af1d9799e6271b5a819bb1
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2022-04-13 10:10:02 -0700

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -109,6 +109,6 @@ setup(
     url="https://github.com/CouncilDataProject/cdp-data",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.0.1",
+    version="0.0.2",
     zip_safe=False,
 )
