0c65aee56cee17111578dcb9270ef6bc6dd00bcb
Eva Maxfield Brown <evamaxfieldbrown@gmail.com>
2022-09-09 18:11:51 -0700

    Add incremental averaging code

---
+++ cdp_data/tests/utils/test_incremental_average.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+from typing import List, Union
+import numpy as np
+
+from cdp_data.utils.incremental_average import update_average, IncrementalAverage
+
+###############################################################################
+
+
+@pytest.mark.parametrize(
+    "additions, averages",
+    [
+        ([1, 1, 1, 1], [1, 1, 1, 1]),
+        ([2, 3, 4], [1.5, 2, 2.5]),
+        ([2, 3, 4, 5, 4, 2], [1.5, 2, 2.5, 3, 19/6, 3]),
+        ([0, 1, 0, 0, 0], [0.5, 2/3, 0.5, 0.4, 1/3]),
+        (
+            [
+                np.array([1, 1, 1]),
+                np.array([1, 1, 1]),
+            ],
+            [
+                np.array([1, 1, 1]),
+                np.array([1, 1, 1]),
+            ],
+        ),
+        (
+            [
+                np.array([2, 2, 2]),
+                np.array([3, 3, 3]),
+            ],
+            [
+                np.array([1.5, 1.5, 1.5]),
+                np.array([2, 2, 2]),
+            ],
+        ),
+        (
+            [
+                np.array([2, 4, 6]),
+                np.array([4, 6, 8]),
+            ],
+            [
+                np.array([1.5, 2.5, 3.5]),
+                np.array([7/3, 11/3, 5]),
+            ],
+        ),
+    ],
+)
+def test_update_average(
+    additions: List[Union[int, np.ndarray]],
+    averages: List[Union[int, float, np.ndarray]]
+) -> None:
+    size = 1
+    for i, addition in enumerate(additions):
+        if i == 0:
+            average = update_average(1, size, addition)
+        else:
+            average = update_average(average, size, addition)
+        
+        size += 1
+
+        # Check that current average is correct
+        np.testing.assert_equal(average, averages[i])
+
+@pytest.mark.parametrize(
+    "additions, averages",
+    [
+        (
+            [
+                np.array([1, 1, 1]),
+                np.array([1, 1, 1]),
+            ],
+            [
+                np.array([1, 1, 1]),
+                np.array([1, 1, 1]),
+            ],
+        ),
+        (
+            [
+                np.array([2, 2, 2]),
+                np.array([3, 3, 3]),
+            ],
+            [
+                np.array([1.5, 1.5, 1.5]),
+                np.array([2, 2, 2]),
+            ],
+        ),
+        (
+            [
+                np.array([2, 4, 6]),
+                np.array([4, 6, 8]),
+            ],
+            [
+                np.array([1.5, 2.5, 3.5]),
+                np.array([7/3, 11/3, 5]),
+            ],
+        ),
+    ],
+)
+def test_incremental_average(
+    additions: List[Union[int, np.ndarray]],
+    averages: List[Union[int, float, np.ndarray]]
+) -> None:
+    # Setup incremental averager
+    inc_avg = IncrementalAverage()
+    inc_avg.add(np.array([1, 1, 1]))
+
+    # Iter additions
+    for i, addition in enumerate(additions):
+        average = inc_avg.add(addition)
+        np.testing.assert_equal(average, averages[i])
---
+++ cdp_data/utils/incremental_average.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from typing import Optional, Union
+
+###############################################################################
+
+AverageableType = Union[int, float, np.ndarray]
+
+
+def update_average(
+    current_average: AverageableType,
+    current_size: int,
+    addition: AverageableType,
+) -> AverageableType:
+    return (current_size * current_average + addition) / (current_size + 1)
+
+
+class IncrementalAverage:
+
+    def __init__(self) -> "IncrementalAverage":
+        self._current_mean = None
+        self._current_size = 0
+
+    @property
+    def current_mean(self) -> Optional[np.ndarray]:
+        return self._current_mean
+    
+    @property
+    def current_size(self) -> int:
+        return self._current_size
+
+    def add(
+        self,
+        addition: np.ndarray,
+    ) -> np.ndarray:
+        # Store initial if nothing yet
+        if self.current_mean is None:
+            self._current_mean = addition
+            self._current_size += 1
+            return self.current_mean
+        
+        # Update if actual addition
+        self._current_mean = update_average(
+            self.current_mean,
+            self.current_size,
+            addition,
+        )
+        self._current_size += 1
+        return self.current_mean
