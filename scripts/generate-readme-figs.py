1c525e9ed4866838c24f97a2630d57f0287e4487
Eva Maxfield Brown <evamaxfieldbrown@gmail.com>
2022-10-10 15:06:45 -0700

    Generate more plots for README

---
+++ scripts/generate-readme-figs.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from cdp_data import CDPInstances, keywords, plots
+plots.set_cdp_plotting_styles()
+
+START_DATETIME = "2022-03-01"
+END_DATETIME = "2022-10-01"
+
+# Compute ngrams for a bunch of infrastructures
+print("Generating header")
+ngram_usage = keywords.compute_ngram_usage_history(
+    [CDPInstances.Seattle, CDPInstances.Portland, CDPInstances.Oakland],
+    start_datetime=START_DATETIME,
+    end_datetime=END_DATETIME,
+)
+grid = plots.plot_ngram_usage_histories(
+    ["police", "housing", "equity"],
+    ngram_usage,
+    lmplot_kws=dict(  # extra plotting params
+        col="ngram",
+        row="infrastructure",
+        hue="ngram",
+        scatter_kws={"alpha": 0.2},
+        aspect=1.6,
+    ),
+)
+grid.savefig("header-keywords-over-time.png")
+
+# Compute ngrams for just Seattle
+print("Generating Seattle ngrams usage")
+seattle_ngram_usage = ngram_usage.loc[ngram_usage.infrastructure == CDPInstances.Seattle]
+grid = plots.plot_ngram_usage_histories(
+    ["police", "housing"],
+    seattle_ngram_usage,
+    lmplot_kws=dict(  # extra plotting params
+        col="ngram",
+        hue="ngram",
+        scatter_kws={"alpha": 0.2},
+        aspect=1.6,
+    ),
+)
+grid.savefig("seattle-keywords-over-time.png")
+
+# Compute semantic sim for just Seattle
+print("Generating Seattle semantic sim")
+semantic_hist = keywords.compute_query_semantic_similarity_history(
+    ["defund the police", "missing middle housing"],
+    CDPInstances.Seattle,
+    start_datetime=START_DATETIME,
+    end_datetime=END_DATETIME,
+)
+grid = plots.plot_query_semantic_similarity_history(
+    semantic_hist,
+    lmplot_kws=dict(  # extra plotting params
+        col="query",
+        hue="query",
+        scatter_kws={"alpha": 0.2},
+        aspect=1.6,
+    ),
+)
+grid.savefig("seattle-semantic-sim-over-time.png")
