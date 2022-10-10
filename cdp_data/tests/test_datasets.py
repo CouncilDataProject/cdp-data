dfa5fee2f78a24386e29d540fcbb0a18a5601d55
Eva Maxfield Brown <evamaxfieldbrown@gmail.com>
2022-10-10 10:58:09 -0700

    feature/add-funcs-and-param-for-easier-data-storage (#15)
    Add funcs and params to make data storage easier

--- cdp_data/tests/test_datasets.py
+++ cdp_data/tests/test_datasets.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from fireo.models import Model
+from numpy.testing import assert_raises
+
 from cdp_data import CDPInstances, datasets
 
 ###############################################################################
@@ -14,5 +17,16 @@ def test_get_session_dataset() -> None:
         store_video=True,
         store_audio=True,
         store_transcript=True,
+        replace_py_objects=True,
     )
     assert len(ds) == 2
+
+    # Assert that no columns are objects
+    for col in ds.columns:
+        assert not isinstance(ds.loc[0][col], Model)
+
+    # Store
+    datasets.save_dataset(ds, "test-outputs-session-dataset.csv")
+    datasets.save_dataset(ds, "test-outputs-session-dataset.parquet")
+    with assert_raises(ValueError):
+        datasets.save_dataset(ds, "blah.bad-suffix")
